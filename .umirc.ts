9c3dcc71a28cb9598ebeffe889a38a33a85fcbbe
gary.xie <1002275364@qq.com>
2021-12-03 15:47:17 +0800

    登录

--- .umirc.ts
+++ .umirc.ts
@@ -4,25 +4,37 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  antd: {},
   routes: [
     {
       path: '/account',
       routes: [
         {
-          path:'/account',
-          redirect:'/account/login'
+          path: '/account',
+          redirect: '/account/login',
         },
         {
           path: 'login',
-          component:'@/pages/account/login/index'
+          component: '@/pages/account/login/index',
         },
         {
           path: 'register',
-          component:'@/pages/account/register/index'
+          component: '@/pages/account/register/index',
         },
       ],
     },
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      { libraryName: 'antd', libraryDirectory: 'lib', style: true },
+      'antd',
+    ],
+    [
+      'babel-plugin-import',
+      { libraryName: '@formily/antd', libraryDirectory: 'lib', style: true },
+    ],
+  ],
 });
--- src/pages/account/login/index.tsx
+++ src/pages/account/login/index.tsx
@@ -1,3 +1,137 @@
-export default ()=>{
-  return <div>test</div>
-}
+import React from 'react';
+import { createForm } from '@formily/core';
+import { createSchemaField, ISchema } from '@formily/react';
+import { Form, FormItem, Input, Password, Submit } from '@formily/antd';
+import { Tabs, Card } from 'antd';
+import VerifyCode from './verify-code'
+import * as ICONS from '@ant-design/icons';
+
+const normalForm = createForm({
+  validateFirst: true,
+});
+
+const phoneForm = createForm({
+  validateFirst: true,
+});
+
+const SchemaField = createSchemaField({
+  components: {
+    FormItem,
+    Input,
+    Password,
+    VerifyCode
+  },
+  scope: {
+    icon(name: string) {
+      return React.createElement(ICONS[name]);
+    },
+  },
+});
+
+const normalSchema: ISchema = {
+  type: 'object',
+  properties: {
+    username: {
+      type: 'string',
+      title: '用户名',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        prefix: '{{icon("UserOutlined")}}',
+      },
+    },
+    password: {
+      type: 'string',
+      title: '密码',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'Password',
+      'x-component-props': {
+        prefix: "{{icon('LockOutlined')}}",
+      },
+    },
+  },
+};
+
+const phoneSchema: ISchema = {
+  type: 'object',
+  properties: {
+    phone: {
+      type: 'string',
+      title: '手机号码',
+      required: true,
+      'x-validator': 'phone',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input',
+      'x-component-props': {
+        prefix: '{{icon("PhoneOutlined")}}',
+      },
+    },
+    verifyCode: {
+      type: 'string',
+      title: '验证码',
+      required: true,
+      'x-decorator': 'FormItem',
+      'x-component': 'VerifyCode',
+      'x-component-props': {
+        prefix: "{{icon('LockOutlined')}}",
+      },
+      'x-reactions': [
+        {
+          dependencies: ['.phone#value', '.phone#valid'],
+          fulfill: {
+            state: {
+              'component[1].readyPost': '{{$deps[0] && $deps[1]}}',
+              'component[1].phoneNumber': '{{$deps[0]}}',
+            },
+          },
+        },
+      ],
+    },
+  },
+};
+
+export default () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        background: '#eee',
+        padding: '40px 0',
+      }}
+    >
+      <Card style={{ width: 400 }}>
+        <Tabs style={{ overflow: 'visible', marginTop: -10 }}>
+          <Tabs.TabPane key="1" tab="账密登录">
+            <Form
+              form={normalForm}
+              layout="vertical"
+              size="large"
+              onAutoSubmit={console.log}
+            >
+              <SchemaField schema={normalSchema} />
+              <Submit block size="large">
+                登录
+              </Submit>
+            </Form>
+          </Tabs.TabPane>
+          <Tabs.TabPane key="2" tab="手机登录">
+            <Form
+              form={phoneForm}
+              layout="vertical"
+              size="large"
+              onAutoSubmit={console.log}
+            >
+              <SchemaField schema={phoneSchema} />
+              <Submit block>
+                登录
+              </Submit>
+            </Form>
+          </Tabs.TabPane>
+        </Tabs>
+      </Card>
+    </div>
+  );
+};
